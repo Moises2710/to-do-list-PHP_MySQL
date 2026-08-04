516eb9fd3f246ed82da65479900b10b92ecd66ed
Moises Coronado <mscoronado630@gmail.com>
2026-08-04 18:50:13 -0400

    feat(web): Add task api routes

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use FastRoute\RouteCollector;
 return FastRoute\simpleDispatcher(function (RouteCollector $r) {
     // Home / Landing page route
     $r->addRoute('GET', '/', ['App\Controllers\HomeController', 'index']);
-    
+
     // Auth routes
     $r->addRoute('GET', '/signup', ['App\Controllers\AuthController', 'signUp']);
     $r->addRoute('POST', '/signup', ['App\Controllers\AuthController', 'register']);
@@ -15,4 +15,10 @@ return FastRoute\simpleDispatcher(function (RouteCollector $r) {
 
     // Dashboard route
     $r->addRoute('GET', '/dashboard', ['App\Controllers\DashboardController', 'index']);
+
+    // Task API routes (AJAX)
+    $r->addRoute('POST', '/tasks', ['App\Controllers\TaskController', 'store']);
+    $r->addRoute('POST', '/tasks/update-status', ['App\Controllers\TaskController', 'updateStatus']);
+    $r->addRoute('POST', '/tasks/edit', ['App\Controllers\TaskController', 'update']);
+    $r->addRoute('POST', '/tasks/delete', ['App\Controllers\TaskController', 'delete']);
 });
